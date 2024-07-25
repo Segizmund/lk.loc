d08defe06365157d3044bf20cae9e8704cecc1b6
Максим Сын Отца Своего <noimxl1@gmail.com>
2024-07-25 11:38:24 +0300

    25.07

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/*SHOW CONTENT RADIO BUTTON*/
+const tabs = document.querySelectorAll('.tab-pane');
+const radios = document.querySelectorAll('input[name="tab-radio"]');
+
+radios.forEach(radio => {
+    radio.addEventListener('change', () => {
+        tabs.forEach(tab => {
+            tab.classList.remove('show', 'active');
+        });
+        const targetTabId = radio.id.replace('-tab', '');
+        document.getElementById(targetTabId).classList.add('show', 'active');
+    });
+});
+
+const passwordInputs = document.querySelectorAll("input[type='password']");
+const eyeIcons = document.querySelectorAll(".show-password img");
+
+eyeIcons.forEach((eyeIcon, index) => {
+    eyeIcon.addEventListener("click", () => {
+        const passwordInput = passwordInputs[index];
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            eyeIcon.src = "img/eye-open.png";
+        } else {
+            passwordInput.type = "password";
+            eyeIcon.src = "img/eye-close.png";
+        }
+    });
+});
